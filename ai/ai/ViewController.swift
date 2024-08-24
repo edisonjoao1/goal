c7b75ab39f1c18b4bfeadb38f7cb417d46668155
edisonjoao1 <40282039+edisonjoao1@users.noreply.github.com>
2024-08-24 16:46:12 -0400

    Add files via upload

---
+++ ai/ai/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ai
+//
+//  Created by Edison E on 8/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .cyan
+        
+        
+    }
+
+
+}
+
---
+++ ai/aiTests/aiTests.swift
@@ -0,0 +1,17 @@
+//
+//  aiTests.swift
+//  aiTests
+//
+//  Created by Edison E on 8/22/24.
+//
+
+import Testing
+@testable import ai
+
+struct aiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
